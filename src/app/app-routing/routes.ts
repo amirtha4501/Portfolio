023e67cd4651dd01e9012a6c8dc3cf81faa7da78
Amirtha <amirtha4501@gmail.com>
2020-10-18 22:37:54 +0530

    Deleted unnecessary components

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -1,14 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from '../home/home.component';
-import { AboutComponent } from '../about/about.component';
-import { PortfolioComponent } from '../portfolio/portfolio.component';
-import { ContactComponent } from '../contact/contact.component';
 import { MainComponent } from '../main/main.component';
 
 export const allroutes: Routes = [
     { path: '', component: MainComponent }, 
-    { path: 'home', component: HomeComponent},
-    { path: 'about', component: AboutComponent},
-    { path: 'portfolio', component: PortfolioComponent},
-    { path: 'contact', component: ContactComponent }
-  ];
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { AboutComponent } from './about/about.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ContactComponent } from './contact/contact.component';
 import { RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    FooterComponent,
-    AboutComponent,
-    PortfolioComponent,
-    ContactComponent,
     MainComponent
   ],
   imports: [
